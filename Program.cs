496c1afe65a2da681b9681e58ea11f66777b678a
yang8320-dot <yang8320@gmail.com>
2026-05-06 20:22:47 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -10,10 +9,6 @@ namespace DrawingApp
 {
     static class Program
     {
-        // 匯入 Windows 底層 API 以強制載入 Native DLL
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr LoadLibrary(string libname);
-
         private static Mutex _mutex;
 
         [STAThread]
@@ -26,10 +21,7 @@ namespace DrawingApp
                 return;
             }
 
-            // 1. 強制讓系統先去 Library 裡面尋找 C++ 底層依賴檔 (如 SQLite)
-            PreLoadNativeLibraries();
-
-            // 2. 攔截一般 C# 程式庫 (如 Newtonsoft.Json) 去 Library 尋找
+            // 註冊組件解析事件作為備用防線
             AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
 
             Application.EnableVisualStyles();
@@ -41,6 +33,7 @@ namespace DrawingApp
                 MessageBox.Show(msg, "系統錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
             };
 
+            // 使用獨立方法啟動，防止 JIT 編譯器在載入 Library 前崩潰
             LaunchApplication();
         }
 
@@ -50,23 +43,7 @@ namespace DrawingApp
             Application.Run(new App_UI_MainForm());
         }
 
-        // 強制系統去 Library 資料夾載入 x64 或 x86 的 Native DLL
-        private static void PreLoadNativeLibraries()
-        {
-            try
-            {
-                string arch = Environment.Is64BitProcess ? "x64" : "x86";
-                string interopPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Library", arch, "SQLite.Interop.dll");
-                
-                if (File.Exists(interopPath))
-                {
-                    LoadLibrary(interopPath);
-                }
-            }
-            catch { /* 忽略底層載入錯誤，交由後續邏輯處理 */ }
-        }
-
-        // 引導 C# 核心套件至 Library 資料夾
+        // 引導 C# 尋找 Library 資料夾中的套件
         private static Assembly ResolveAssembly(object sender, ResolveEventArgs args)
         {
             try
@@ -75,7 +52,10 @@ namespace DrawingApp
                 string assemblyName = new AssemblyName(args.Name).Name;
                 string assemblyPath = Path.Combine(folderPath, assemblyName + ".dll");
                 
-                if (File.Exists(assemblyPath)) return Assembly.LoadFrom(assemblyPath);
+                if (File.Exists(assemblyPath))
+                {
+                    return Assembly.LoadFrom(assemblyPath);
+                }
             }
             catch { }
             return null;
